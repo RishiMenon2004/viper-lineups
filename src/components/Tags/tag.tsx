472c70b711460943e0d11a0fdae3a0a016c74faa
RishiMenon2004 <rishimenon82004@gmail.com>
2023-03-11 00:40:58 +0530

    handle undefined tag

--- src/components/Tags/tag.tsx
+++ src/components/Tags/tag.tsx
@@ -1,14 +1,29 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useQuery } from "../../convex/_generated/react"
 
 function Tag({isSmall, id}: {isSmall?: boolean, id:any}) {
 	
 	const tagQuery = useQuery("tags/getTagByID", id)
-	const tag = tagQuery !== undefined ? tagQuery.at(0) : {id: 0, displayText: ""} 
+	const tag = tagQuery?.at(0)
+
+	const tagContents = () => {
+		if (tag !== undefined) {
+			return (<>
+				<img src={`/tag_icons/${tag.id}.png`} className='icon' alt="tag icon"/>
+				{tag.displayText}
+			</>)
+		}
+
+		return (<>
+			<FontAwesomeIcon icon={faSpinner}/>
+			Getting
+		</>)
+	}
 	
 	return (
 		<div className={"tag" + (isSmall ? ' small' : '')}>
-			<img src={`/tag_icons/${tag?.id}.png`} className='icon' alt="tag icon"/>
-			{tag?.displayText}
+			{tagContents()}
 		</div>
 	)
 }
