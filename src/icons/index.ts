be5140a0d304f3519899b355a7d859b905e80f46
RishiMenon2004 <rishimenon82004@gmail.com>
2023-03-12 00:51:36 +0530

    delete unused files

--- src/icons/index.ts
+++
@@ -1,8 +0,0 @@
-let snakebite = '/tag_icons/snakebite.png';
-let poisonCloud = '/tag_icons/poison_cloud.png';
-let vipersPit = '/tag_icons/vipers_pit.png';
-let toxicScreen = '/tag_icons/toxic_screen.png';
-let attack = '/tag_icons/attack.png';
-let defend = '/tag_icons/defend.png';
-
-export {snakebite, poisonCloud, vipersPit, toxicScreen, attack, defend}
