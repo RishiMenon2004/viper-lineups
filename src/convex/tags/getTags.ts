c492d7989ea9e9441e8fb22e65f159c07aeb3f65
RishiMenon2004 <rishimenon82004@gmail.com>
2023-03-12 02:55:03 +0530

    order by alphabetical order to soothe my ocd

--- src/convex/tags/getTags.ts
+++ src/convex/tags/getTags.ts
@@ -1,5 +1,15 @@
 import { query } from "../_generated/server";
 
 export default query(async ({db}) => {
-	return await db.query("tags").collect()
+	return await (await db.query("tags").collect()).sort((a, b) => {
+		if (a.id > b.id) {
+			return 1
+		}
+
+		if (a.id < b.id) {
+			return -1
+		}
+
+		return 0
+	})
 })
